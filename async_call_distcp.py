cf6609228ef2162d258627b0279e39678bb6b967
Sam <samueltong@live.cn>
2017-07-21 17:57:12 +0800

    del async_call_distcp.py

--- async_call_distcp.py
+++
@@ -1,9 +0,0 @@
-from celery import Celery
-from subprocess import call
-
-app = Celery('call_distcp', backend = 'amqp://', broker = 'amqp://')
-
-@app.task
-def async_call_distcp(source_data_block_url, dis_data_block_url):
-    ret_val = call(["hadoop", "distcp", source_data_block_url, dis_data_block_url])
-    return ret_val
